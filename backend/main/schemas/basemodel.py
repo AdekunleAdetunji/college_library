c3336a3a801c665cc5df0ec396978e8b67688cfe
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-20 00:50:03 +0100

    feat: Create basemodel module for all ORMs

---
+++ backend/main/schemas/basemodel.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" The basemodel module containing the basemodel class of all tables"""
+from uuid import uuid4
+from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import UUID
+
+
+class Basemodel():
+    """The base class for all tables"""
+    id = Column(UUID, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize a new table entry"""
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.id = uuid4()
+        for k, v in kwargs.items():
+            if hasattr(self, k) and k not in ["created_at", "updated_at",
+                                              "id"]:
+                setattr(self,  k, v)
+
+    def to_dict(self):
+        """return a dictionary representation of the table entry"""
+        dict_rep = self.__dict__.copy()
+        dict_rep.pop("_sa_instance_state")
+        dict_rep["created_at"] = dict_rep["created_at"].strftime(
+            "%Y-%m-%dT%H:%M:%S"
+        )
+        dict_rep["updated_at"] = dict_rep["updated_at"].strftime(
+            "%Y-%m-%dT%H:%M:%S"
+        )
+        return dict_rep
+
+    def __repr__(self) -> str:
+        """an unambigous string representation of the instance"""
+        return f"{type(self).__name__}({self.to_dict()})"
+
+    def __str__(self) -> str:
+        """human readable representation of the table entry"""
+        return f"{type(self).__name__}({self.to_dict()})"
