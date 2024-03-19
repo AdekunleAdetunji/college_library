5c242fc271a6cfc09852c7c4dace4f3612f16c17
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-20 00:51:06 +0100

    feat: Create staff ORM module

---
+++ backend/main/schemas/staff.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains the staff ORM used to model the university student table
+"""
+from ..cursor.base import Base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy_utils import EmailType
+
+
+class Staff(Base):
+    """ORM for the university student table"""
+    __tablename__ = "staffs"
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    middlename = Column(String(50))
+    staff_no = Column(String(20), nullable=False)
+    email = Column(EmailType(100), nullable=False)
+    phone_no = Column(String(20), nullable=False)
