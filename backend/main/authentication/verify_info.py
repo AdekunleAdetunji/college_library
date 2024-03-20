ec413a8c92709ec73bc3cd8a6a6ec7f50ea7c499
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-20 18:53:33 +0100

    feat: add check to verify liberarian info

--- backend/main/authentication/verify_info.py
+++ backend/main/authentication/verify_info.py
@@ -2,21 +2,31 @@
 """
 This module contains function to verify login info
 """
+from ..schemas.liberarian import Liberarian
+from ..cursor.cursor import Cursor
 from fastapi import HTTPException
 from fastapi import status
 from os import getenv
 
+INV_INFO_EXC = HTTPException(detail="invalid username or password",
+                             status_code=status.HTTP_401_UNAUTHORIZED)
+
 
 def verify_info(supplied_username: str, supplied_password: str,
-                admin: bool = True):
+                is_admin: bool = False, is_liberarian: bool = False,
+                is_user: bool = False, cursor: Cursor | None = None):
     """function to verify the supplied username and password"""
-    if admin:
+    if is_admin:
         saved_username = getenv("ADMIN_USERNAME")
         saved_password = getenv("ADMIN_PASSWORD")
         if (supplied_username != saved_username
                 or supplied_password != saved_password):
-            raise HTTPException(detail="invalid username or password",
-                                status_code=status.HTTP_401_UNAUTHORIZED)
+            raise INV_INFO_EXC
+    elif is_liberarian:
+        liberarian = cursor.get_staff(Liberarian, supplied_username)
+        if (liberarian.staff_no != supplied_username
+                or liberarian.password != supplied_password):
+            raise INV_INFO_EXC
     else:
-        # Write logic for other sign in asides admin
+        # Write logic for user sign in
         pass
