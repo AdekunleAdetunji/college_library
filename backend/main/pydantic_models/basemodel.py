da25a243adbb8aa30c42d299073f7f8240483b8f
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-23 14:11:58 +0100

    feat: Create a new UniBaseModel and LibBaseModel class

--- backend/main/pydantic_models/basemodel.py
+++ backend/main/pydantic_models/basemodel.py
@@ -3,10 +3,25 @@
 This module contains the basemodel pydantic model for all other pydantic models
 """
 from datetime import datetime
+from pydantic import BaseModel
+from pydantic import EmailStr
 from uuid import UUID
 
 
-class Base():
+class UniBaseModel(BaseModel):
+    """Staff data validator class"""
+    firstname: str
+    lastname: str
+    middlename: str
+    email: EmailStr
+    uni_id: str
+    phone_no: str
+
+    class Config:
+        from_attributes = True
+
+
+class LibBaseModel(UniBaseModel):
     """Pydantic model containing common attributes shared by other models"""
     id: UUID
     created_at: datetime
