06393f9a655bc486c65b1d8b180e271c07624f92
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-23 13:59:52 +0100

    feat: Add lib account manager router and login router to library app

--- backend/main/liberarian/liberarian.py
+++ backend/main/liberarian/liberarian.py
@@ -3,37 +3,13 @@
 This module contains the admin application that handles all routing relating to
 the liberarian
 """
+from .routers.account import account_router
 from .routers.token import lib_login_router
-from ..authentication.verify_token import oauth2_scheme
-from ..authentication.verify_token import verify_token
-from ..cursor.cursor import Cursor
-from ..pydantic_models.liberarian import LiberarianModelOut
-from ..schemas.liberarian import Liberarian
-from fastapi import Depends
 from fastapi import FastAPI
-from fastapi import HTTPException
-from fastapi import status
-from typing import Annotated
+
 
 liberarian = FastAPI()
 
 
-@liberarian.get("/liberarian", status_code=status.HTTP_200_OK,
-                response_model=LiberarianModelOut,
-                response_model_exclude=["password"])
-def get_lib_info(staff_no: str, token: Annotated[str, Depends(oauth2_scheme)],
-                 lib_cursor: Cursor = Depends(Cursor())):
-    """router to handle retrieval of liberarian info"""
-    token_staff_no = verify_token(token=token)
-    if token_staff_no["sub"] != staff_no:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="access denied")
-
-    liberarian = lib_cursor.get_staff(Liberarian, staff_no)
-    if not liberarian:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="liberarian does not exist")
-    return liberarian
-
-
 liberarian.include_router(lib_login_router)
+liberarian.include_router(account_router)
