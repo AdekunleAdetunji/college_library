f6e4febda73a7cbef0b9d9226de2046c05572c23
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-23 13:54:34 +0100

    feat: Add a new variable

--- backend/main/authentication/auth_var.py
+++ backend/main/authentication/auth_var.py
@@ -7,4 +7,5 @@ from datetime import timedelta
 ALGORITHM = "HS256"
 access_token_expires_admin = timedelta(days=1)
 access_token_expires_lib = timedelta(days=1)
+access_token_expires_user = timedelta(days=1)
 SECRET_KEY = "d05a69af3da8c5b3bacf4841fa9649ba395c8047a4fec8a0a59a630122ba6619"
