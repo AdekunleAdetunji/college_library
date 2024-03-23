d68880649837d1067ec0c0b951dcd923246fbbb2
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-23 13:53:48 +0100

    feat: Rename staff_id variable to uni_id

--- backend/main/admin/routers/liberarian.py
+++ backend/main/admin/routers/liberarian.py
@@ -8,6 +8,7 @@ import string
 from ...authentication.verify_token import oauth2_scheme
 from ...authentication.verify_token import verify_token
 from ...cursor.cursor import Cursor
+from ...microservices.gen_code import gen_random_pass
 from ...pydantic_models.liberarian import LiberarianModelOut
 from ...pydantic_models.liberarian import LiberarianModelIn
 from ...schemas.liberarian import Liberarian
@@ -22,20 +23,15 @@ from typing import Annotated
 admin_liberarian = APIRouter()
 
 
-def generate_random_password():
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(secrets.choice(alphabet) for i in range(8))
-    return password
-
-
 @admin_liberarian.get("/get-liberarian", response_model=LiberarianModelOut,
-                      status_code=status.HTTP_200_OK)
-async def get_liberarian(staff_no: str,
+                      status_code=status.HTTP_200_OK,
+                      response_model_exclude=["password"])
+async def get_liberarian(uni_id: str,
                          token: Annotated[str, Depends(oauth2_scheme)],
                          lib_cursor: Cursor = Depends(Cursor())):
     """route to get info of a registered liberarian"""
     verify_token(token)
-    liberarian = lib_cursor.get_staff(Liberarian, staff_no)
+    liberarian = lib_cursor.get(Liberarian, uni_id)
     if not liberarian:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="liberarian does not exist")
@@ -44,23 +40,23 @@ async def get_liberarian(staff_no: str,
 
 @admin_liberarian.post("/add-liberarian", response_model=LiberarianModelOut,
                        status_code=status.HTTP_201_CREATED)
-async def register_liberarian(staff_no: str,
+async def register_liberarian(uni_id: str,
+                              password: str,
                               token: Annotated[str, Depends(oauth2_scheme)],
                               uni_cursor: Cursor = Depends(Cursor(
                                   db="university")),
                               lib_cursor: Cursor = Depends(Cursor())):
     """register a new liberarian to the liberary"""
     verify_token(token)
-    staff = uni_cursor.get_staff(Staff, staff_no)
+    staff = uni_cursor.get(Staff, uni_id)
     if not staff:
         raise HTTPException(detail="staff not found",
                             status_code=status.HTTP_404_NOT_FOUND)
 
-    password = generate_random_password()
     staff_dict = {"firstname": staff.firstname, "lastname": staff.lastname,
                   "middlename": staff.middlename, "email": staff.email,
                   "phone_no": staff.phone_no, "password": password,
-                  "staff_no": staff.staff_no}
+                  "uni_id": staff.uni_id}
     liberarian = LiberarianModelIn(**staff_dict)
     try:
         liberarian = lib_cursor.new(Liberarian, **liberarian.model_dump())
