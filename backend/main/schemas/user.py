bf8377fa0fc0c2fc240dc0f80a795e1390975a84
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-23 14:17:28 +0100

    feat: Create library user ORM

---
+++ backend/main/schemas/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains the ORM used to interact with library user table
+"""
+from ..cursor.base import Base
+from ..schemas.basemodel import Basemodel
+from sqlalchemy import Column
+from sqlalchemy import Boolean
+from sqlalchemy import String
+from sqlalchemy_utils import PasswordType
+
+
+class User(Basemodel, Base):
+    """ORM to interact with the library user table"""
+    __tablename__ = "users"
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    middlename = Column(String(50), nullable=True)
+    uni_id = Column(String(20), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(PasswordType(schemes="md5_crypt"), nullable=True)
+    phone_no = Column(String(20), nullable=False)
+    is_staff = Column(Boolean, nullable=False)
