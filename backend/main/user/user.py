0c161f4671ac2261b83b18f386ddff9ff6c7cdd6
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-23 14:18:09 +0100

    feat: Add new routers to the user app

--- backend/main/user/user.py
+++ backend/main/user/user.py
@@ -3,6 +3,14 @@
 This module contains the student application that handles all routing relating
 to the student
 """
+from .routers.account import user_account_router
+from .routers.registration import registration_router
+from .routers.token import user_login_router
 from fastapi import FastAPI
 
 user = FastAPI()
+
+
+user.include_router(user_account_router)
+user.include_router(registration_router)
+user.include_router(user_login_router)
