03a4c62ca58e66388396643df85882f912f7c372
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-24 15:20:36 +0100

    openapi: Rename tag metadata variable

--- backend/main/liberarian/liberarian.py
+++ backend/main/liberarian/liberarian.py
@@ -6,14 +6,14 @@ the liberarian
 from main.openapi_meta import app_metadata
 from .routers.account import account_router
 from .routers.token import lib_login_router
-from ..openapi_meta.tag import tags_metadata
+from ..openapi_meta.tag import liberarian_tags_metadata
 from fastapi import FastAPI
 
 
-del tags_metadata[0]
+#del tags_metadata[0]
 liberarian = FastAPI(title="College Library (Librarian App)",
                      contact=app_metadata.contact,
-                     openapi_tags=tags_metadata)
+                     openapi_tags=liberarian_tags_metadata)
 
 
 liberarian.include_router(lib_login_router)
--- backend/main/user/user.py
+++ backend/main/user/user.py
@@ -7,12 +7,12 @@ from main.openapi_meta import app_metadata
 from .routers.account import user_account_router
 from .routers.registration import registration_router
 from .routers.token import user_login_router
-from ..openapi_meta.tag import tags_metadata
+from ..openapi_meta.tag import user_tags_metadata
 from fastapi import FastAPI
 
 user = FastAPI(title="College Library (User App)",
                contact=app_metadata.contact,
-               openapi_tags=tags_metadata)
+               openapi_tags=user_tags_metadata)
 
 
 user.include_router(user_account_router)
