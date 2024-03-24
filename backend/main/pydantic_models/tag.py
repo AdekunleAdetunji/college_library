5c5f88ebe4433c5a46634964aed94e341e3c20c2
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-24 06:49:17 +0100

    feat: Create module to manage openapi tags

---
+++ backend/main/pydantic_models/tag.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module contains the tag model to hold all tag names
+"""
+from enum import Enum
+
+sign_up_desc = """Operations with signing up. The sign-up **logic** is here."""
+reset_pass_desc = """Operations to reset password in case of a forgotten
+password. The reset password **logic** is here.
+"""
+ind_info_desc = """Operation to manage the retrieval of information about a
+liberarian/staff or a liberary user.
+"""
+
+tags_metadata = [
+    {"name": "Sign up",
+     "description": sign_up_desc},
+    {"name": "Reset password",
+     "description": reset_pass_desc},
+    {"name": "Get Individual Information",
+     "description": ind_info_desc},
+]
+
+
+class Tags(Enum):
+    get_ind_info = "Get Individual Information"
+    reset_pass = "Reset password"
+    sign_in = "Sign in"
+    sign_up = "Sign up"
