9482914b92bf1544e5a8cdde070bbf4f2a9019f8
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-24 07:03:34 +0100

    feat: Add tag metadata to liberarian app

--- backend/main/liberarian/liberarian.py
+++ backend/main/liberarian/liberarian.py
@@ -5,10 +5,11 @@ the liberarian
 """
 from .routers.account import account_router
 from .routers.token import lib_login_router
+from ..pydantic_models.tag import tags_metadata
 from fastapi import FastAPI
 
 
-liberarian = FastAPI()
+liberarian = FastAPI(openapi_tags=tags_metadata)
 
 
 liberarian.include_router(lib_login_router)
