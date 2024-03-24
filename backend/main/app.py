2f18779a33f9f11675f1543a51d3a27156b3e494
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-24 07:24:35 +0100

    feat: Move app metadat variables to app_metadatamodule

--- backend/main/app.py
+++ backend/main/app.py
@@ -7,42 +7,14 @@ librarian, and student applications are mounted.
 """
 from .admin import admin
 from .liberarian import liberarian
+from .openapi_meta import app_metadata
 from .user import user
 from fastapi import FastAPI
 
-# Overview description of the College Library application
-description = """
-# College Library
-
-## Overview
-The College Library application is designed to efficiently manage library
-resources for educational institutions. It simplifies book management for
-librarians while providing students with the ability to check book availability
-and make reservations.
-
-## Subapplications
-Three apps are mounted on this app:
-- **Admin**: Controls all actions related to administration.
-  [Admin Documentation](http://localhost:8000/admin/docs)
-
-- **Librarian**: Controls actions for librarians.
-  [Librarian Documentation](http://localhost:8000/librarian/docs)
-
-- **User**: Controls actions for library users.
-  [User Documentation](http://localhost:8000/user/docs)
-"""
-
-# Contact information for the developer
-contact = {
-    "name": "Adekunle Adetunji",
-    "url": "https://www.linkedin.com/in/adetunji-adekunle-835755233/",
-    "email": "adekunleadetunjiwilson@gmail.com"
-}
-
 # Create the FastAPI application
 app = FastAPI(title="College Library",
-              description=description,
-              contact=contact,
+              description=app_metadata.description,
+              contact=app_metadata.contact,
               version="0.0.1")
 
 app.mount(path="/admin", app=admin.admin)
---
+++ backend/main/openapi_meta/app_metadata.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains the metadata variables of the main app
+"""
+# Overview description of the College Library application
+description = """
+# College Library
+
+## Overview
+The College Library application is designed to efficiently manage library
+resources for educational institutions. It simplifies book management for
+librarians while providing students with the ability to check book availability
+and make reservations.
+
+## Subapplications
+Three apps are mounted on this app:
+- **Admin**: Controls all actions related to administration.
+  [Admin Documentation](http://localhost:8000/admin/docs)
+
+- **Librarian**: Controls actions for librarians.
+  [Librarian Documentation](http://localhost:8000/librarian/docs)
+
+- **User**: Controls actions for library users.
+  [User Documentation](http://localhost:8000/user/docs)
+"""
+
+# Contact information for the developer
+contact = {
+    "name": "Adekunle Adetunji",
+    "url": "https://www.linkedin.com/in/adetunji-adekunle-835755233/",
+    "email": "adekunleadetunjiwilson@gmail.com"
+}
