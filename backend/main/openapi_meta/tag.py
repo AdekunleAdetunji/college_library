9cc44fb5de14ac838ac89f1cf5dd147a611fcca5
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-26 07:44:03 +0100

    openapi: Add metadata for the book routers

--- backend/main/openapi_meta/tag.py
+++ backend/main/openapi_meta/tag.py
@@ -12,21 +12,27 @@ ind_info_desc = """Operations to manage the retrieval of information about a
 liberarian/staff or a liberary user.
 """
 
+book_operations = """Routers to manage all activities relating to a book."""
+
 tags_metadata = [
     {"name": "Sign up",
      "description": sign_up_desc},
+    {"name": "Book Operations",
+     "description": book_operations},
     {"name": "Get Individual Information",
      "description": ind_info_desc},
     {"name": "Reset password",
-     "description": reset_pass_desc}
+     "description": reset_pass_desc},
 ]
 
 admin_tags_metadata = [tags_metadata[0], tags_metadata[1]]
-liberarian_tags_metadata = [tags_metadata[1], tags_metadata[2]]
+liberarian_tags_metadata = [tags_metadata[1], tags_metadata[3],
+                            tags_metadata[2]]
 user_tags_metadata = tags_metadata
 
 class Tags(Enum):
     get_ind_info = "Get Individual Information"
+    book = "Book Operations"
     reset_pass = "Reset password"
     sign_in = "Sign in"
     sign_up = "Sign up"
