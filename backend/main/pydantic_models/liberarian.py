095ce68d0eb0f30173bfd38933afffd0e776e11d
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-26 07:24:37 +0100

    feat: Add a new model for validating input data for liberarian registration

--- backend/main/pydantic_models/liberarian.py
+++ backend/main/pydantic_models/liberarian.py
@@ -2,9 +2,15 @@
 """
 This module contains the pydantic model for the liberian data validation
 """
+from .basemodel import BaseIn
 from .basemodel import LibBaseModel
 from .basemodel import UniBaseModel
 
+
+class LibRegModel(BaseIn):
+    """Liberarian registration model"""
+    uni_id: str
+    new_password: str
 
 class LiberarianModelOut(LibBaseModel):
     """Liberarian data validator"""
