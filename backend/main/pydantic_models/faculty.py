fb9dfb8b9da8d3a2f3cb69554be63040fc7ee13b
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-26 07:22:13 +0100

    feat: Create pydantic model for faculty table data validation

---
+++ backend/main/pydantic_models/faculty.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains pydantic module to validate input and output data into
+the library faculty table
+"""
+from .basemodel import BaseIn
+from .basemodel import BaseOut
+
+
+class FacultyModelIn(BaseIn):
+    "model to validate data going into the table"
+    uni_id: str
+    name: str | None = None
+
+
+
+class FacultyModelOut(FacultyModelIn, BaseOut):
+    """model to validate model going out of the table"""
+    pass
