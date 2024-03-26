f41db5b7348b34c4b12074835e8c05eab47d4947
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-26 07:20:56 +0100

    feat: Create pydantic model for author table data validation

---
+++ backend/main/pydantic_models/author.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains the pydantic model for validating input and output data
+into the author table
+"""
+from .basemodel import BaseIn
+from .basemodel import BaseOut
+from pydantic import EmailStr
+
+
+class AuthorModelIn(BaseIn):
+    """model to validate author input data"""
+    firstname: str
+    lastname: str
+    middlename: str
+    email: EmailStr | None = None
+
+class AuthorModelOut(BaseOut, AuthorModelIn):
+    """model to validate author output data"""
+    pass
