2d5584563eb6c583b9d0fc173d92e3d10236ec1d
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-26 07:25:34 +0100

    feat: Add a new model for validating input data for new user registration

--- backend/main/pydantic_models/user.py
+++ backend/main/pydantic_models/user.py
@@ -3,9 +3,16 @@
 This module contains pydantic model that validates user data in and out of the
 university database
 """
+from .basemodel import BaseIn
 from .basemodel import UniBaseModel
 from .basemodel import LibBaseModel
 
+
+class UserRegModel(BaseIn):
+    """Model for new user library registration"""
+    uni_id: str
+    is_staff: bool = False
+    new_password: str
 
 class UserModelIn(UniBaseModel):
     """User model to validate data going into the user table"""
