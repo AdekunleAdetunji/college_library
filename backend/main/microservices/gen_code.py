6bd6347f399c43bc93c57f9e366d14c767fdc839
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-26 07:30:47 +0100

    feat: Converted async function to sync

--- backend/main/microservices/gen_code.py
+++ backend/main/microservices/gen_code.py
@@ -5,7 +5,7 @@ This module contains a fuction for generating a random code
 import string
 import secrets
 
-async def gen_random_code():
+def gen_random_code():
     """function to handle generation of random"""
     code = "".join(secrets.choice(string.digits) for i in range(6))
     return code
