212d77b074f6b6c22f65c96d565247e68e5bf580
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-27 16:09:13 +0100

    openapi: Add documentation for sign in tag

--- backend/main/openapi_meta/tag.py
+++ backend/main/openapi_meta/tag.py
@@ -4,10 +4,15 @@ This module contains the tag model to hold all tag names
 """
 from enum import Enum
 
+
+sign_in_desc = """Operation(s) to sign in. The sign in **logic** is here"""
+
 sign_up_desc = """Operations with signing up. The sign-up **logic** is here."""
+
 reset_pass_desc = """Operations to reset password in case of a forgotten
 password. The reset password **logic** is here.
 """
+
 ind_info_desc = """Operations to manage the retrieval of information about a
 librarian/staff or a liberary user.
 """
@@ -17,6 +22,8 @@ book_operations = """Routers to manage all activities relating to a book."""
 tags_metadata = [
     {"name": "Sign up",
      "description": sign_up_desc},
+    {"name": "Sign in",
+     "description": sign_in_desc},
     {"name": "Book Operations",
      "description": book_operations},
     {"name": "Get Individual Information",
@@ -25,10 +32,12 @@ tags_metadata = [
      "description": reset_pass_desc},
 ]
 
-admin_tags_metadata = [tags_metadata[0], tags_metadata[1]]
-librarian_tags_metadata = [tags_metadata[1], tags_metadata[3],
-                            tags_metadata[2]]
+admin_tags_metadata = [tags_metadata[0], tags_metadata[1], tags_metadata[3]]
+librarian_tags_metadata = [tags_metadata[1],
+                           tags_metadata[3],
+                           tags_metadata[2]]
 user_tags_metadata = tags_metadata
+
 
 class Tags(Enum):
     get_ind_info = "Get Individual Information"
