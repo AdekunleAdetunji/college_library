d92b96c40ee08828063c23e42a422384a9c7ab9f
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-27 16:14:09 +0100

    feat: Add a new schema field (quantity)

--- backend/main/schemas/book.py
+++ backend/main/schemas/book.py
@@ -2,12 +2,15 @@
 """
 This module contains the ORM used to interact with the library books table
 """
-from ..cursor.base import Base
+from .author import Author
 from .basemodel import Basemodel
+from .faculty import Faculty
 from .junctions.author_book import author_book
 from .junctions.faculty_book import faculty_book
+from ..cursor.base import Base
 from sqlalchemy import Column
 from sqlalchemy import Date
+from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
@@ -16,12 +19,12 @@ class Book(Basemodel, Base):
     """Books ORM"""
     __tablename__ = "books"
     title = Column(String(100), nullable=False)
-    summary = Column(String(1000), nullable=False)
+    description = Column(String(1000), nullable=False)
     uni_id = Column(String(13), nullable=False, unique=True)
     publisher = Column(String(50), nullable=True)
     publish_year = Column(Date(), nullable=True)
+    quantity = Column(Integer, nullable=False)
     authors = relationship("Author", secondary=author_book,
                            back_populates="books")
     faculties = relationship("Faculty", secondary=faculty_book,
                              back_populates="books")
-    
