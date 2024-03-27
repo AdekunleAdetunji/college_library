264773d61e68eb4706f7b153d777a442d6490d4c
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-27 16:05:39 +0100

    feat: Add task scheduler to celery

--- backend/main/microservices/celery/app.py
+++ backend/main/microservices/celery/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains the app to manage celery tasks"""
 from celery import Celery
+from celery.schedules import crontab
 
 
 task_path = "main.microservices.celery.tasks"
@@ -10,3 +11,14 @@ app = Celery(
     broker="redis://localhost:6379/",
     include=[task_path]
 )
+
+app.conf.beat_schedule = {
+    "check_reserve": {
+        "task": "main.microservices.celery.tasks.check_reserves",
+        "schedule": 30
+    },
+    "whitelist": {
+        "task": "main.microservices.celery.tasks.whitelist",
+        "schedule": 30
+    }
+}
