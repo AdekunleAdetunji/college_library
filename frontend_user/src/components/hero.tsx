6e314a118ff674ff5b6695e1f209aa71309df7b0
Olawole Olawale Martins <olawalemart@gmail.com>
2024-03-29 23:44:32 +0100

    fixed bug with navigation in home page

--- frontend_user/src/components/hero.tsx
+++ frontend_user/src/components/hero.tsx
@@ -2,10 +2,8 @@
 import React from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 const Hero = () => {
-  const router = useRouter();
   return (
     <main className="w-full h-full bg-[url('/library.jpg')] flex flex-col gap-6 justify-center items-center relative">
       <div className="w-full h-full absolute left-0 top-0 bg-black/30"></div>
@@ -15,17 +13,15 @@ const Hero = () => {
         </p>
         <p className="text-xl font-bold">Shelby Foote</p>
       </div>
-      <div className="flex gap-8">
-        <Button onClick={() => router.push("/login")} size="lg">
-          Sign In
-        </Button>
-        <Button
-          onClick={() => router.push("/books")}
-          size="lg"
-          variant="secondary"
-        >
-          Explore
-        </Button>
+      <div className="flex gap-8 relative">
+        <Link href="/login">
+          <Button size="lg">Sign In</Button>
+        </Link>
+        <Link href="/books">
+          <Button size="lg" variant="secondary">
+            Explore
+          </Button>
+        </Link>
       </div>
     </main>
   );
