5102412a013df62db4982b10226d03416180168b
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-03-29 17:30:24 +0100

    feat: Create borrows table ORM

---
+++ backend/main/schemas/borrow.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains the borrow ORM class
+"""
+from .basemodel import Basemodel
+from ..cursor.base import Base
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import DateTime
+from sqlalchemy import String
+from sqlalchemy import UUID
+
+
+class Borrow(Basemodel, Base):
+    """borrow table ORM"""
+    __tablename__ = "borrows"
+    book_uuid = Column(UUID, ForeignKey("books.id"), nullable=False)
+    librarian_uuid = Column(UUID, ForeignKey("librarians.id"),
+                          nullable=False)
+    user_uuid = Column(UUID, ForeignKey("users.id"),  nullable=False)
+    expire_time = Column(DateTime, nullable=False)
+    active = Column(Boolean, nullable=False, default=True)
