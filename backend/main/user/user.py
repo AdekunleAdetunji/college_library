cfc1bca16853ee0d14b9b409adfec80eeb20f02f
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-04-01 08:45:34 +0100

    feat: Add explore route to freely view all library books

--- backend/main/user/user.py
+++ backend/main/user/user.py
@@ -11,12 +11,39 @@ from .routers.book.reserves import reserves_router
 from .routers.faculty import faculty_router
 from .routers.registration import registration_router
 from .routers.token import user_login_router
+from ..cursor.cursor import Cursor
+from ..openapi_meta.tag import Tags
 from ..openapi_meta.tag import user_tags_metadata
+from ..pydantic_models.book import BookModelOut
+from ..schemas.book import Book
+from fastapi import Depends
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
 
 user = FastAPI(title="College Library (User App)",
                contact=app_metadata.contact,
                openapi_tags=user_tags_metadata)
+
+
+@user.get("/explore", response_model=list[BookModelOut],
+          status_code=status.HTTP_200_OK, tags=[Tags.book])
+async def explore(lib_cursor: Cursor = Depends(Cursor())):
+    """router to fetch all the books available in the library"""
+
+    return lib_cursor.all(Book)
+
+
+@user.get("/explore/{book_id}", status_code=status.HTTP_200_OK,
+          tags=[Tags.book], response_model=BookModelOut)
+async def explore_a_book(book_id: str, lib_cursor: Cursor = Depends(Cursor())):
+    """router to get a particular book"""
+    book = lib_cursor.get(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="book not found")
+
+    return book
 
 
 user.include_router(user_account_router)
